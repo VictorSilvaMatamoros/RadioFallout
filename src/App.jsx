6b0db4fa48270920011729a9728040263999d818
VictorSilvaMatamoros <120045384+VictorSilvaMatamoros@users.noreply.github.com>
2025-05-01 18:22:26 +0200

    Revert "Actualicacione pagina inicial"
    This reverts commit 20e8ba5ac39ab56ab40c719533c6d1afed025b3d.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 // src/App.jsx
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Routes, Route, NavLink } from 'react-router';
+import { BrowserRouter as Router, Routes, Route, NavLink  } from 'react-router';
 
-import ScrollHorizontal from './components/ScrollHorizontal'; // ✅ IMPORTAMOS ESTE
+import RadioOnline from './components/RadioOnline';
 import Fallout3 from './views/Fallout3';
 import FalloutNewVegas from './views/FalloutNewVegas';
 import Fallout4 from './views/Fallout4';
@@ -15,33 +15,34 @@ function App() {
         <h1>📻 Radio Galaxia</h1>
 
         <nav>
-          <ul style={{ display: 'flex', gap: '20px', justifyContent: 'center', listStyle: 'none', marginBottom: '20px' }}>
-            <li>
-              <NavLink to="/" className={({ isActive }) => isActive ? "nav-link active" : "nav-link"}>
-                Inicio
-              </NavLink>
-            </li>
-            <li>
-              <NavLink to="/fallout3" className={({ isActive }) => isActive ? "nav-link active" : "nav-link"}>
-                Fallout 3
-              </NavLink>
-            </li>
-            <li>
-              <NavLink to="/newvegas" className={({ isActive }) => isActive ? "nav-link active" : "nav-link"}>
-                Fallout New Vegas
-              </NavLink>
-            </li>
-            <li>
-              <NavLink to="/fallout4" className={({ isActive }) => isActive ? "nav-link active" : "nav-link"}>
-                Fallout 4
-              </NavLink>
-            </li>
-          </ul>
-        </nav>
+  <ul style={{ display: 'flex', gap: '20px', justifyContent: 'center', listStyle: 'none', marginBottom: '20px' }}>
+    <li>
+      <NavLink to="/" className={({ isActive }) => isActive ? "nav-link active" : "nav-link"}>
+        Inicio
+      </NavLink>
+    </li>
+    <li>
+      <NavLink to="/fallout3" className={({ isActive }) => isActive ? "nav-link active" : "nav-link"}>
+        Fallout 3
+      </NavLink>
+    </li>
+    <li>
+      <NavLink to="/newvegas" className={({ isActive }) => isActive ? "nav-link active" : "nav-link"}>
+        Fallout New Vegas
+      </NavLink>
+    </li>
+    <li>
+      <NavLink to="/fallout4" className={({ isActive }) => isActive ? "nav-link active" : "nav-link"}>
+        Fallout 4
+      </NavLink>
+    </li>
+  </ul>
+</nav>
+
 
         {/* Rutas */}
         <Routes>
-          <Route path="/" element={<ScrollHorizontal />} /> {/* 🔄 AQUÍ CAMBIAMOS */}
+          <Route path="/" element={<RadioOnline />} />
           <Route path="/fallout3" element={<Fallout3 />} />
           <Route path="/newvegas" element={<FalloutNewVegas />} />
           <Route path="/fallout4" element={<Fallout4 />} />
